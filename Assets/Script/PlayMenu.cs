80df36d88b942ccdcefea97ddcbeba8ebba091ce
Martin gdn <44martin.g@gmail.com>
2020-10-13 18:33:32 +0200

    feat add basic start game

---
+++ Assets/Script/PlayMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeSceneGame(string sceneName){
+         Debug.Log("Changement de scene de " + gameObject.name + " à " + sceneName);
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+    }
+}
