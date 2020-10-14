068be4ef0bee6f36eb941eb0c4a6687adb6f3482
Martin gdn <44martin.g@gmail.com>
2020-10-14 08:26:24 +0200

    feat add quit game

--- Assets/Script/PlayGame.cs
+++ Assets/Script/PlayGame.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayMenu : MonoBehaviour
+public class PlayGame : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
---
+++ Assets/Script/QuitGame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class QuitGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("escape")){
+            if (EditorApplication.isPlaying){
+                EditorApplication.isPlaying = false;
+            }else{
+                Application.Quit();
+            }
+        }
+    }
+
+    public void Quit(){
+        if (EditorApplication.isPlaying){
+            EditorApplication.isPlaying = false;
+        }else{
+            Application.Quit();
+        }
+    }
+}
