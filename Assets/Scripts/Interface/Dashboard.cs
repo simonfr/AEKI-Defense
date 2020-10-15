3b54e8537e14867ee29254b6f2bab91c83c02c09
Simon PEROLS <simon@perols.dev>
2020-10-15 09:04:31 +0200

    fix: change script paths

---
+++ Assets/Scripts/Interface/Dashboard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dashboard : MonoBehaviour
+{
+
+    public bool startButtonAvailable;
+    public GameObject cases;
+    public List<GameObject> towers;
+
+    void Start()
+    { }
+
+    bool AllTowersPlaced() {
+        foreach(GameObject tower in towers) {
+
+        }
+
+        return true;
+    }
+
+}
---
+++ Assets/Scripts/Interface/Drag.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour
+{
+    private bool isDragging;
+    private bool mouseOver;
+
+    public GameObject cases;
+
+    void OnMouseOver()
+    {
+        mouseOver = true;
+    }
+
+    void OnMouseExit()
+    {
+        mouseOver = false;
+    }
+
+    public void OnMouseDown()
+    {
+        isDragging = true;
+    }
+
+    public void OnMouseUp()
+    {
+        isDragging = false;
+
+        GameObject nearestObject = searchNearestCase();
+
+        Vector3 directionToTarget = nearestObject.transform.position - this.transform.position;
+        float dSqrToTarget = directionToTarget.sqrMagnitude;
+
+        if(dSqrToTarget <= 10f)
+        {
+            this.transform.position = nearestObject.transform.position;
+        }
+    }
+
+    void Update()
+    {
+        if (isDragging) {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePosition);
+        }
+
+        if (isDragging || mouseOver) {
+            GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f, .5f);
+        } else {
+            GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+        }
+    }
+
+    GameObject searchNearestCase() {
+        Transform bestTarget = null;
+        float closestDistanceSqr = Mathf.Infinity;
+
+        foreach(Transform potentialTarget in cases.transform)
+        {
+            Vector3 directionToTarget = potentialTarget.position - this.transform.position;
+            float dSqrToTarget = directionToTarget.sqrMagnitude;
+
+            if(dSqrToTarget < closestDistanceSqr)
+            {
+                closestDistanceSqr = dSqrToTarget;
+                bestTarget = potentialTarget;
+            }
+        }
+
+        return bestTarget.gameObject;
+    }
+
+}
