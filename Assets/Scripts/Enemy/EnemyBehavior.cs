ff9bf496ec9415bdb22ba7843998b4229637d894
Riwal <riwal.lefaou7@gmail.com>
2020-10-15 08:16:32 +0200

    Add path for termite

---
+++ Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    [SerializeField] private float speed = 0f;
+
+    private Vector3 direction; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + direction * speed;
+    }
+
+    public void ChangeDirection(DirectionEnum dir){
+        switch (dir) {
+            case DirectionEnum.UP:
+                direction = Vector3.up;
+                break;
+            case DirectionEnum.DOWN:
+                direction = Vector3.down;
+                break;
+            case DirectionEnum.LEFT:
+                direction = Vector3.left;                
+                break;
+            case DirectionEnum.RIGHT:
+                direction = Vector3.right;
+                break;
+        }    
+    }
+}
---
+++ Assets/Scripts/Enemy/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed = 2f;
+    public float delta = 5f;
+    private Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = startPosition;
+        v.y += delta * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}
